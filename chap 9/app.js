e90c71a84b888ac66406aa1e1c1d99968b7a7ef6
Wahaj khan <154734596+wahaj906@users.noreply.github.com>
2024-04-25 03:31:11 +0500

    Add files via upload

---
+++ chap 9/app.js
@@ -0,0 +1,14 @@
+
+/* Q 1 */
+
+let studentNames1 = []; 
+studentNames1.push("Haider");
+studentNames1.push("qasim");
+console.log(studentNames1);
+
+/* Q 2 */
+
+let studentNames2 = {names: []}; 
+studentNames2.names.push("Hussain");
+studentNames2.names.push("Qamber");
+console.log(studentNames2);
